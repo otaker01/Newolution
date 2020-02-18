b141976a30dc4e06ba9b3ad5a21d7179013d7588
Otaker01 <48328063+otaker01@users.noreply.github.com>
2020-02-18 15:17:37 +0100

    first commit

--- src/assets/toolkit/scripts/toolkit.js
+++ src/assets/toolkit/scripts/toolkit.js
@@ -30,3 +30,42 @@ $(function(){
 		$(".fouc").css('opacity',1);
 	},200);
 });
+//   $(".modern").on('change', function() {
+// 	  console.log("changeing")
+//   })
+
+//   $( "body" ).change(function() {
+// 	console.log( "Handler for .change() called." );
+//   });
+//   console.log(window.screenY)
+
+
+// $(document).scroll(function() {
+//     console.log($(document).scrollTop());
+// })
+
+// My code
+
+$(".inner-modern_part-2").click( function() {
+	$(".circle").toggleClass("rotate");
+	$(".day").toggleClass("active")
+	$(".night").toggleClass("active")
+  })
+  
+
+// scroll to top
+  $(document).ready(function(){
+	$('.ScrollTop').click(function() {
+	  $('html, body').animate({scrollTop: 0}, 800);
+	return false;
+	  });
+  });
+
+//   watch
+$(document).ready(function (){
+	$(".watch").click(function (){
+		$('html, body').animate({
+			scrollTop: $("#intro").offset().top
+		}, 800);
+	});
+});
